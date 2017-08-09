38d72b5c11a293848b6db0c6e4a277b763a717fe
Gikuyu Nderitu <gikyunderitu@gmail.com>
2017-08-09 08:42:25 -0400

    Update remote repo with changes

--- build/src/app/components/ProjectsView/ProjectsView.js
+++ build/src/app/components/ProjectsView/ProjectsView.js
@@ -10,7 +10,7 @@ class ProjectsView extends Component {
 			<div id={this.props.id} className={this.props.className}>
 				<Header size="large">{'Projects'}</Header>
 				<p>{`Here are my projects, go ahead, take a look!`}</p>
-				<Button> Take a look !</Button>
+				<Button primary> Take a look !</Button>
 			</div>
 		)
 	}
